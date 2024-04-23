d0d1d49afff45dd8a3ffc1f7fc09d5a3f8f7a75f
ShijinA530 <130336477+ShijinA530@users.noreply.github.com>
2024-04-23 14:13:26 +0530

    Update app.js

--- Backend/tempauth/app.js
+++ Backend/tempauth/app.js
@@ -13,19 +13,7 @@ app.use(cookieParser());
 // CORS configuration to allow multiple origins and handle preflight requests
 const allowedOrigins = ['http://localhost:5173', 'https://de-vote1.vercel.app'];
 app.use(cors({
-    origin: function(origin, callback) {
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                      'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true,
-    optionsSuccessStatus: 200 // For legacy browser support
+  origin: 'https://de-vote1.vercel.app' // specify the origin of your frontend
 }));
 
 // Database connection
